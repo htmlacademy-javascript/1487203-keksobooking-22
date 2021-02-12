a4416c83112669e1b333b44a675b6da92cd82618
Anna <alexia.elm@gmail.com>
2021-02-13 01:22:39 +0300

    Конкатенация заменена на template string и изменён принцип генерации  массива features

--- js/main.js
+++ js/main.js
@@ -52,12 +52,15 @@ const getRandomArrayElement = (elemets) => {
   return elemets[getRandomInteger(0, elemets.length - 1)];
 };
 
+
+
 const createAdvertisement =() => {
-  const avatar = 'img/avatars/user0' + getRandomInteger(1, 8) + '.png';
+  let avatarNum = getRandomInteger(1, 8);
+  const avatar = 'img/avatars/user0 ${avatarNum} .png';
   author: 'avatar';
 
   const title = 'Добро пожаловать!';
-  const address = getRandomNumber(35.65000, 35.70000, 5) + "," + getRandomNumber(139.70000, 139.80000, 5);
+  const address = 'x ${,} y';
   const price = getRandomNumber(1, 10);
   const type = getRandomArrayElement(TYPES);
   const rooms = getRandomNumber(1, 10);
@@ -66,13 +69,14 @@ const createAdvertisement =() => {
   const checkout = getRandomArrayElement(TIME);
   const features = FEATURES.splice(0, getRandomInteger(1, FEATURES.length));
   const description = getRandomArrayElement(DESCRIPTIONS);
-  const photos = PHOTOS.splice(0, getRandomInteger(1, PHOTOS.length));
-  offer: title+ '' + address + '' + price + '' + type + '' + rooms + '' + guests + '' + checkin +
-   '' + checkout + '' + features + '' + description + '' + photos;
+
+  const shuffledPhotos = shuffle(PHOTOS);
+  const photos = shuffledPhotos.splice(0, getRandomInteger(1, PHOTOS.length));
+  offer: 'title ${address} ${price} ${type} ${rooms} ${guests} ${checkin} ${checkout} ${features} ${description} photos';
 
   const x = getRandomNumber(35.65, 35.70, 2);
   const y = getRandomNumber(139.70, 139.80, 2);
-  location: x + ":" + y;
+  location: 'x ${:} y';
 }
 
 const freshAdvertisements = new Array(ADVERTISEMENTS_COUNT).fill(null).map(() => createAdvertisement());
